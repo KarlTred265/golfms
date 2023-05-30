8cb65e460e73355a1643c407b7018873bf9b7bda
KarlTred265 Tred <tred26559@gmail.com>
2023-05-30 16:55:44 +0000

    add Navbar links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Social Golf World</h1>} />
+          <Route exact path="/about us" render={() => <h1>About us</h1>} />
+          <Route exact path="/contact us" render={() => <h1>Contact us</h1>} />
+          <Route exact path="/settings" render={() => <h1>Settings</h1>} />
+          <Route exact path="/sign in" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/sign up" render={() => <h1>Sign up</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
+          </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,69 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/golflogo.jpg'
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
- return (
-    <Navbar bg="light" expand="lg">
-     <Container>
-      <Navbar.Brand>
-       <img src={logo} alt="logo" height="45" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-       <Nav className="ml-auto text-right">
-        <Nav.Link>
-        <i class="fa-solid fa-golf-ball-tee"></i>Golf World
-        </Nav.Link>
-        <Nav.Link>
-        <i class="fa-solid fa-right-to-bracket"></i>Sign in
-        </Nav.Link>
-        <Nav.Link>
-        <i class="fa-solid fa-user-plus"></i>Sign up
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-   </Container>
-  </Navbar>
- );
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fa-solid fa-golf-ball-tee"></i>Social Golf World
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/about us"
+            >
+             <i className="fa-solid fa-address-card"></i>About us 
+            </NavLink>
+            <NavLink
+              to="/contact us "
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+            <i className="fa-solid fa-envelope"></i>Contact us  
+            </NavLink>
+            <NavLink
+              to="/settings "
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+            <i className="fa-solid fa-gear"></i>Settings
+            </NavLink>
+            <NavLink
+              to="/sign in "
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+            <i className="fa-solid fa-right-to-bracket"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/sign up "
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+            <i className="fa-solid fa-user-plus"></i>Sign up
+            </NavLink> 
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
